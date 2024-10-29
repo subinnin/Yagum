69606cca9650cb813add39edb5980986a65416f0
subin <ppttkang98@naver.com>
2024-10-29 17:09:37 +0900

    수빈 연결

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
+import Home from "./pages/Home"
 import Doma1 from "./pages/doma1"
 import Doma2 from "./pages/doma2"
 import Menu from "./pages/menu"
@@ -10,6 +11,7 @@ function App() {
     <div>
     <Router>
       <Routes>
+        <Route path='/home' element={<Home />}></Route>
         <Route path='/' element={<Doma1 />}></Route>
         <Route path='/doma2' element={<Doma2 />}></Route>
         <Route path='/menu' element={<Menu />}></Route>
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -41,12 +41,12 @@ const Home = () => {
     });
     return (
         <div>
-            <header className={title}>냉장고를 야금야금</header>
-            <div className={fridge}>
-                <div className={`${door} ${left - door}`}>
-                    <div className={foodBox}>
-                        <div className={`${textAreaRight} ${textArea}`}>원하는 스타일을 선택하세요.</div>
-                        <div className={`${inputAreaRight} ${inputArea} ${checkbox - buttons}`}>
+            <header className={styles.title}>냉장고를 야금야금</header>
+            <div className={styles.fridge}>
+                <div className={`${styles.door} ${styles.left} ${styles.door}`}>
+                    <div className={styles.foodBox}>
+                        <div className={`${styles.textAreaRight} ${styles.textArea}`}>원하는 스타일을 선택하세요.</div>
+                        <div className={`${styles.inputAreaRight} ${styles.inputArea} ${styles.checkbox} ${styles.buttons}`}>
                             <input type="checkbox" id="korean" name="food" value="한식"></input>
                             <label for="korean">한식</label>
                             <input type="checkbox" id="chinese" name="food" value="중식"></input>
@@ -59,33 +59,33 @@ const Home = () => {
                             <label for="noPreference">상관없음</label>
                         </div>
                     </div>
-                    <div className={foodBox}>
-                        <div className={`${textArea} ${textAreaRight}`}>요리 가능 시간을 선택하세요.</div>
-                        <div className={`${inputArea} ${inputAreaRight}`}>
-                            <input id="inputRange" type="range" min="5" max="60" className={inputRange} value="5"></input>
-                            <p className={time}><span id="time">5</span>분</p>
-                            <button id="nextPageBtn" className={nextPageBtn}>선택완료</button>
+                    <div className={styles.foodBox}>
+                        <div className={`${styles.textArea} ${styles.textAreaRight}`}>요리 가능 시간을 선택하세요.</div>
+                        <div className={`${styles.inputArea} ${styles.inputAreaRight}`}>
+                            <input id="inputRange" type="range" min="5" max="60" className={styles.inputRange} value="5"></input>
+                            <p className={styles.time}><span id="time">5</span>분</p>
+                            <button id="nextPageBtn" className={styles.nextPageBtn}>선택완료</button>
                         </div>
                     </div>
                 </div>
-                <div className={handle} id="open-handle"></div>
-                <div className={`${foodBoxLeft1} ${foodBoxLeft}`}>
-                    <div className={textArea}>냉장고 재료를 3가지 이상 골라주세요.</div>
-                    <div className={inputArea}>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
+                <div className={styles.handle} id="open-handle"></div>
+                <div className={`${styles.foodBoxLeft1} ${styles.foodBoxLeft}`}>
+                    <div className={styles.textArea}>냉장고 재료를 3가지 이상 골라주세요.</div>
+                    <div className={styles.inputArea}>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
                     </div>
                 </div>
-                <div className={`${foodBoxLeft2} ${foodBoxLeft}`}>
-                    <div className={textArea}>조리 도구를 한 개 이상 적어주세요.</div>
-                    <div className={`${inputArea}  ${checkbox - buttons}`}>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
-                        <input type="text" className={inputFood}></input>
+                <div className={`${styles.foodBoxLeft2} ${styles.foodBoxLeft}`}>
+                    <div className={styles.textArea}>조리 도구를 한 개 이상 적어주세요.</div>
+                    <div className={`${styles.inputArea}  ${styles.checkbox} ${styles.buttons}`}>
+                        <input type="text" className={styles.nputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
+                        <input type="text" className={styles.inputFood}></input>
                         <input type="checkbox" id="nomatter" name="food" value="양식"></input>
                         <label for="nomatter">상관없음</label>
                     </div>
