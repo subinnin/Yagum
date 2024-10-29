d56c57ec0ee3837d139cac30a640c56d91c76699
subin <ppttkang98@naver.com>
2024-10-29 09:24:16 +0900

    App.js에 doma1,2추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Routes} from 'react-router-dom'
+
+import Doma1 from "./pages/doma1"
+import Doma2 from "./pages/doma2"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path='/' element={<Doma1 />}></Route>
+        <Route path='/doma2' element={<Doma2 />}></Route>
+      </Routes>
+
   );
 }
 
