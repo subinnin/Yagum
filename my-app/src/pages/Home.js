6beb5ed5a80be37c3f4acd487360a4d2a7d4b35a
jomim2 <wjddla7374@naver.com>
2024-11-01 16:23:40 +0900

    imgupdate

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -40,44 +40,44 @@ const Home = () => {
         }
     };
     /* ====== 수빈 작성 24.10.30 17:40~*/
-    const [foodCheckBoxes, setFoodCheckBoxes] = useState({
-        optionKorean : false,
-        optionChinese : false,
-        optionJapanese : false,
-        optionWestern : false,
-        optionNone : false
-    })
+    // const [foodCheckBoxes, setFoodCheckBoxes] = useState({
+    //     optionKorean : false,
+    //     optionChinese : false,
+    //     optionJapanese : false,
+    //     optionWestern : false,
+    //     optionNone : false
+    // })
 
-    /*체크박스 값 변경 핸들러 */
-    const handleFoodCheckBoxChange = (event) =>{
-        const {name, checked} = event.target;
-        setFoodCheckBoxes((prevFoodCheckBoxes)=>({
-            ...prevFoodCheckBoxes,
-            [name]: checked,
-        }))
-    }
-    console.log(foodCheckBoxes)
+    // /*체크박스 값 변경 핸들러 */
+    // const handleFoodCheckBoxChange = (event) =>{
+    //     const {name, checked} = event.target;
+    //     setFoodCheckBoxes((prevFoodCheckBoxes)=>({
+    //         ...prevFoodCheckBoxes,
+    //         [name]: checked,
+    //     }))
+    // }
+    // console.log(foodCheckBoxes)
 
-    //true인 키의 이름을 한글로 변환
-    const foodCheckLabels = Object.entries(foodCheckBoxes)
-        .filter(([key,value])=>value)
-        .map(([key])=>{
-            switch (key){
-                case 'optionKorean':
-                    return '한식'
-                case 'optionChinese':
-                    return '중식'
-                case 'optionJapanese':
-                    return '일식'
-                case 'optionWestern':
-                    return '양식'
-                case 'optionNone':
-                    return '없음'
-                default:
-                    return '';
-            }
-        })
-    console.log(foodCheckLabels)
+    // //true인 키의 이름을 한글로 변환
+    // const foodCheckLabels = Object.entries(foodCheckBoxes)
+    //     .filter(([key,value])=>value)
+    //     .map(([key])=>{
+    //         switch (key){
+    //             case 'optionKorean':
+    //                 return '한식'
+    //             case 'optionChinese':
+    //                 return '중식'
+    //             case 'optionJapanese':
+    //                 return '일식'
+    //             case 'optionWestern':
+    //                 return '양식'
+    //             case 'optionNone':
+    //                 return '없음'
+    //             default:
+    //                 return '';
+    //         }
+    //     })
+    // console.log(foodCheckLabels)
     const handleRangeChange = (event) => {
         setCookTime(event.target.value);
     };
@@ -86,38 +86,86 @@ const Home = () => {
 
     /// ===== 기범 작성 시작 ====================================================
     //전체 전달값
-    const [totalElement, setTotalElement] = useState({});
-    
+    const [totalElement, setTotalElement] = useState({
+        ingredient: [],
+        tool: '',
+        style: '',
+        time: 0
+    });
+
     //전체 전달값(tool)
     const setTotalTool = (event) => {
         const selectedtool = event.target.name;
-        setTotalElement({
-            tool: selectedtool,
-            check_food_style : foodCheckLabels,
-        })
+        setTotalElement((prev)=>({
+            ...prev,
+            tool: selectedtool
+        }))
     }
-    //전체 전달값(tool)
+    ////
+    //전체 전달값(style)
+    const setTotalStyle = (event) => {
+        const selectedStyle = event.target.name;
+        setTotalElement((prev)=>({
+            ...prev,
+            style: selectedStyle
+        }))
+    }
+    ////
+    //전체 전달값(ingredient)
+    const handleIngredientChange = (index, value) => {
+        setTotalElement(prevTotalElement => {
+          const updatedIngredients = [...prevTotalElement.ingredient];
+          updatedIngredients[index] = value;
+          return {
+            ...prevTotalElement,
+            ingredient: updatedIngredients.filter(Boolean),
+          };
+        });
+      };
+    ////
 
-    
-    //체크박스 하나만
+    //체크박스 하나만(Tools)
     const [toggleTools, setToggleTools] = useState({
-        microwave: false,
-        oven: false,
-        none: false,
-        nomatter: false,
+        전자레인지: false,
+        오븐: false,
+        없음: false,
+        상관없음: false,
     });
 
     const toolsToggling = (event) => {
         const { name, checked } = event.target;
 
         setToggleTools({
-            microwave: false,
-            oven: false,
-            none: false,
-            nomatter: false,
+            전자레인지: false,
+            오븐: false,
+            없음: false,
+            상관없음: false,
             [name]: checked,
         });
     };
+    ////
+    //체크박스 하나만(Styles)
+    const [toggleStyles, setToggleStyles] = useState({
+        한식: false,
+        중식: false,
+        일식: false,
+        양식: false,
+        상관없음: false
+    });
+
+    const stylesToggling = (event) => {
+        const { name, checked } = event.target;
+    
+        setToggleStyles({
+            한식: false,
+            중식: false,
+            일식: false,
+            양식: false,
+            상관없음: false,
+            [name]: checked,
+        });
+    }
+    ////
     /// =======기범작성 끝 =====================================================
 
 
@@ -150,7 +198,7 @@ const Home = () => {
                 clearInterval(interval)
             }, 5000);
 
-            // 냉장고 재료 밸류값 
+            //냉장고 재료 밸류값 
             const resultInput = [inputValue1, inputValue2, inputValue3, inputValue4, inputValue5, inputValue6];
             let sendInput = [];
             // 밸류값이 있으면 sendInput 에 넣기
@@ -159,7 +207,7 @@ const Home = () => {
                     sendInput.push(resultInput[i]);
                 }
             })
-            // 잔고로 보내는 코드
+            //잔고로 보내는 코드
             const submit = async () => {
                 try {
                     const response = await fetch("http://localhost:8000/data/", {
@@ -175,11 +223,12 @@ const Home = () => {
                     const data = await response.json();
                     console.log(data.message);
                 } catch (err) {
-                    console.error("ERROR :", err);
+                    console.error("ERROR:", err);
                 }
             }
             submit();
         }
+        console.log(totalElement)
     };
 
     return (
@@ -190,50 +239,15 @@ const Home = () => {
                     <div className={styles.foodBox}>
                         <div className={`${styles.textAreaRight} ${styles.textArea}`}>원하는 스타일을 선택해주세요.</div>
                         <div className={`${styles.inputAreaRight} ${styles.inputArea} ${styles.checkbox_buttons}`}>
-                        <input 
-                                type="checkbox" 
-                                id="korean" 
-                                name="optionKorean" 
-                                checked={foodCheckBoxes.optionKorean} 
-                                onChange={handleFoodCheckBoxChange} 
-                                value="한식" 
-                            />
+                            <input type="checkbox" id="korean" name="한식" value="한식" checked={toggleStyles.한식} onClick={stylesToggling} onChange={setTotalStyle}/>
                             <label htmlFor="korean">한식</label>
-                            <input 
-                                type="checkbox" 
-                                id="chinese" 
-                                name="optionChinese" 
-                                checked={foodCheckBoxes.optionChinese}
-                                onChange={handleFoodCheckBoxChange}  
-                                value="중식" 
-                            />
+                            <input type="checkbox" id="chinese" name="중식" value="중식" checked={toggleStyles.중식} onClick={stylesToggling} onChange={setTotalStyle}/>
                             <label htmlFor="chinese">중식</label>
-                            <input 
-                                type="checkbox" 
-                                id="japanese" 
-                                name="optionJapanese" 
-                                checked={foodCheckBoxes.optionJapanese} 
-                                onChange={handleFoodCheckBoxChange}
-                                value="일식" 
-                            />
+                            <input type="checkbox" id="japanese" name="일식" value="일식" checked={toggleStyles.일식} onClick={stylesToggling} onChange={setTotalStyle}/>
                             <label htmlFor="japanese">일식</label>
-                            <input 
-                                type="checkbox" 
-                                id="western" 
-                                name="optionWestern" 
-                                checked={foodCheckBoxes.optionWestern}
-                                onChange={handleFoodCheckBoxChange} 
-                                value="양식" 
-                            />
+                            <input type="checkbox" id="western" name="양식" value="양식"  checked={toggleStyles.양식} onClick={stylesToggling} onChange={setTotalStyle}/>
                             <label htmlFor="western">양식</label>
-                            <input 
-                                type="checkbox" 
-                                id="noPreference" 
-                                name="optionNone" 
-                                checked={foodCheckBoxes.optionNone} 
-                                onChange={handleFoodCheckBoxChange}
-                                value="상관없음" 
-                            />
+                            <input type="checkbox" id="noPreference" name="상관없음" value="상관없음" checked={toggleStyles.상관없음} onClick={stylesToggling} onChange={setTotalStyle}/>
                             <label htmlFor="noPreference">상관없음</label>
                         </div>
                     </div>
@@ -259,30 +273,32 @@ const Home = () => {
                 <div className={`${styles.foodBoxLeft1} ${styles.foodBoxLeft}`}>
                     <div className={styles.textArea}>냉장고 재료를 3가지 이상 골라주세요.</div>
                     <div className={styles.inputArea}>
-                        <input type="text" className={styles.inputFood} value={inputValue1} onChange={(e) => { setInputValue1(e.target.value) }} />
-                        <input type="text" className={styles.inputFood} value={inputValue2} onChange={(e) => { setInputValue2(e.target.value) }} />
-                        <input type="text" className={styles.inputFood} value={inputValue3} onChange={(e) => { setInputValue3(e.target.value) }} />
-                        <input type="text" className={styles.inputFood} value={inputValue4} onChange={(e) => { setInputValue4(e.target.value) }} />
-                        <input type="text" className={styles.inputFood} value={inputValue5} onChange={(e) => { setInputValue5(e.target.value) }} />
-                        <input type="text" className={styles.inputFood} value={inputValue6} onChange={(e) => { setInputValue6(e.target.value) }} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(0, e.target.value)} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(1, e.target.value)} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(2, e.target.value)} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(3, e.target.value)} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(4, e.target.value)} />
+                        <input type="text" className={styles.inputFood} onChange={(e) => handleIngredientChange(5, e.target.value)} />
                     </div>
                 </div>
+
                 <div className={`${styles.foodBoxLeft2} ${styles.foodBoxLeft}`}>
                     <div className={styles.textArea}>조리 도구를 한 개 이상을 선택해주세요.</div>
                     <div className={`${styles.inputArea} ${styles.checkbox_buttons}`}>
-                    <input type="checkbox" id="microwave" name="microwave" value="전자레인지" checked={toggleTools.microwave} onChange={setTotalTool} onClick={toolsToggling}/>
+                        <input type="checkbox" id="microwave" name="전자레인지" value="전자레인지" checked={toggleTools.전자레인지} onChange={setTotalTool} onClick={toolsToggling} />
                         <label htmlFor="microwave">전자레인지</label>
-                        <input type="checkbox" id="oven" name="oven" value="오븐" checked={toggleTools.oven} onChange={setTotalTool} onClick={toolsToggling}/>
+                        <input type="checkbox" id="oven" name="오븐" value="오븐" checked={toggleTools.오븐} onChange={setTotalTool} onClick={toolsToggling} />
                         <label htmlFor="oven">오븐</label>
-                        <input type="checkbox" id="none" name="none" value="없음" checked={toggleTools.none} onChange={setTotalTool} onClick={toolsToggling}/>
+                        <input type="checkbox" id="none" name="없음" value="없음" checked={toggleTools.없음} onChange={setTotalTool} onClick={toolsToggling} />
                         <label htmlFor="none">없음</label>
-                        <input type="checkbox" id="nomatter" name="nomatter" value="상관없음" checked={toggleTools.nomatter} onChange={setTotalTool} onClick={toolsToggling}/>
+                        <input type="checkbox" id="nomatter" name="상관없음" value="상관없음" checked={toggleTools.상관없음} onChange={setTotalTool} onClick={toolsToggling} />
                         <label htmlFor="nomatter">상관없음</label>
                     </div>
                 </div>
             </Fridge>
         </div>
     );
+    // console.log(totalElement)
 };
 
 export default Home;
