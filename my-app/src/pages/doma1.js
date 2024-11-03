674c6eeb7d601dc8f36a751783d3068eaf802f29
jomim2 <wjddla7374@naver.com>
2024-11-03 12:46:52 +0900

    Add files via upload

--- my-app/src/pages/doma1.js
+++ my-app/src/pages/doma1.js
@@ -18,10 +18,10 @@ const Doma1 = () =>{
         <header className = {styles.title}>냉장고를 야금야금</header>
     <div className={styles.cuttingBoard}>
       <div className={`${styles.side} ${styles.leftSide}`}>
-        <div onClick={navigateToMenu} className={styles.sideInside}><p className={styles.writingMode}>선택 레시피</p></div>
+        <div onClick={navigateToMenu} className={styles.sideInside}><p className={styles.writingMode}>레시피 선택</p></div>
       </div>
       <div className={`${styles.side} ${styles.rightSide}`}>
-        <div onClick={navigateToDoma2} className={styles.sideInside}><p className={styles.writingMode}>만들기 음식 </p></div>
+        <div onClick={navigateToDoma2} className={styles.sideInside}><p className={styles.writingMode}>음식 만들기</p></div>
       </div>
       <div className={styles.cookingArea}>
         <div className={`${styles.menu} ${styles.menuTitle}`}>김치찌개</div>
@@ -29,29 +29,27 @@ const Doma1 = () =>{
       <div className={styles.middleArea}>
         <div className={styles.leftColumn}>
           <div className={`${styles.item} ${styles.ingredientArea}`}>
-            <p>필요한 재료</p>
-            <div className={styles.cookingIngredientItem}></div>
-            <div className={styles.cookingIngredientItem}></div>
-            <div className={styles.cookingIngredientItem}></div>
+            <p className={styles.titleNeedFood}>필요한 재료</p>
+            <p className={styles.needFood}>김치</p>
+            <p className={styles.needFood}>돼지고기</p>
+            <p className={styles.needFood}>양파</p>
           </div>
         </div>
         <div className={styles.rightColumn}>
           <div className={`${styles.item} ${styles.cookingTimeArea}`}>
-            <p>조리 예상 시간</p>
-            <label><input type="number" /> 시간</label>
+            <p className={styles.titleNeedFood}>조리 예상 시간</p>
+            <p className={styles.titleNeedFood2}><input type="number" /> 시간</p>
           </div>
         </div>
       </div>
       <div className={styles.bottomArea}>
         <div className={`${styles.item} ${styles.cookingMethodArea}`}>
-          <p>만드는 법</p>
-          <div className={styles.cookingMethodItem}></div>
-          <div className={styles.cookingMethodItem}></div>
-          <div className={styles.cookingMethodItem}></div>
-          <div className={styles.cookingMethodItem}></div>
-          <div className={styles.cookingMethodItem}></div>
-          <div className={styles.cookingMethodItem}></div>
-          
+          <p className={styles.titleNeedFood}>만드는 법</p>
+          <p className={styles.makeFood}>1. 김치를 썰어서 담아둡니다</p>
+          <p className={styles.makeFood}>2. 돼지고기를 썰어서 담아둡니다.</p>
+          <p className={styles.makeFood}>3. 국물을 끓이고 재료를 넣습니다.</p>
+          <p className={styles.makeFood}>5. 10 분간 끓여줍니다.</p>
+          <p className={styles.makeFood}>6. 요리가 완성되었습니다.</p>
         </div>
       </div>
     </div>
